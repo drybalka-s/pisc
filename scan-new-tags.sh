f4360cec81133c038693020871cb3418eb16f325
Владимир Капистка <v.kapistka@vkteam.ru>
2026-02-09 00:31:38 +0300

    refactor: major restructure of YARA

--- scan-new-tags.sh
+++ scan-new-tags.sh
@@ -32,7 +32,7 @@ EMOJI_INFO='\U1F4A1' # bulb
 # it is important for run *.sh by ci-runner
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # get exported var with default value if it is empty
-: "${OUT_DIR:=/tmp}"
+: "${PISC_OUT_DIR:=/tmp}"
 # check debug mode to debug child scripts and external tools
 DEBUG_SKOPEO=''
 if [[ "$-" == *x* ]]; then
@@ -40,15 +40,15 @@ if [[ "$-" == *x* ]]; then
 fi
 
 # result of skopeo inspect of source image (don't remove)
-JSON_FILE=$OUT_DIR'/inspect.json'
+JSON_FILE=$PISC_OUT_DIR'/inspect.json'
 # result of skopeo inspect of new-version images
-JSON_TEMP_FILE=$OUT_DIR'/inspect-temp.json'
+JSON_TEMP_FILE=$PISC_OUT_DIR'/inspect-temp.json'
 # result output
-RES_FILE=$OUT_DIR'/scan-new-tags.result'
+RES_FILE=$PISC_OUT_DIR'/scan-new-tags.result'
 #temp version file after sorting
-SORT_FILE=$OUT_DIR'/scan-new-tags.sort'
+SORT_FILE=$PISC_OUT_DIR'/scan-new-tags.sort'
 #temp version file before sorting
-TMP_FILE=$OUT_DIR'/scan-new-tags.tmp'
+TMP_FILE=$PISC_OUT_DIR'/scan-new-tags.tmp'
 rm -f $JSON_TEMP_FILE $RES_FILE $SORT_FILE $TMP_FILE
 
 SKOPEO_AUTH_FLAG=''
